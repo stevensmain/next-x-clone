7830d58e7d5aeccfd57be37e842e651dfab82321
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2024-03-22 15:45:45 -0400

    button-server fix

--- components/auth-button-server.tsx
+++ components/auth-button-server.tsx
@@ -1,11 +1,11 @@
-import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
-import AuthButtonClient from "./auth-button-client";
+import AuthButtonClient from './auth-button-client'
 
 export default async function AuthButtonServer() {
-  const supabase = createServerComponentClient({ cookies });
-  const { data } = await supabase.auth.getSession();
+  const supabase = createServerComponentClient({ cookies })
+  const { data } = await supabase.auth.getSession()
 
-  return <AuthButtonClient session={data.session} />;
+  return <AuthButtonClient />
 }
